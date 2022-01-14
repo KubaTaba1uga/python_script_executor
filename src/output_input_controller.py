45716ef368dd63fe6417d7d1a8e1e09e23c5d91d
KubaTaba1uga <jakub@buczynski.net.pl>
2022-01-14 14:39:49 +0100

    change InputOutput for InputOutputController

---
+++ src/output_input_controller.py
@@ -0,0 +1,107 @@
+import abc
+
+
+class BaseDescriptor(abc.ABC):
+    def __set_name__(self, cls, name):
+        self.name = name
+
+    @abc.abstractmethod
+    def __set__(self, instance, value_tuple):
+        """Receive shell and value to allow
+        OutputInputController children
+        for controlling shell process"""
+
+        shell, value = value_tuple
+
+        instance.__dict__[self.name] = value
+
+    @abc.abstractmethod
+    def __get__(self, instance, cls):
+        return instance.__dict__[self.name]
+
+
+class OutputInputController(abc.ABC):
+    """Class responsible for handling terminal streams.
+            Responsibilities description:
+
+                receiving stdin and deciding what to do
+                    with process that input was received from
+
+                receiving stdout and deciding what to do
+                    with process that output was received from
+
+                receiving stderr and deciding what to do
+                    with process that error was received from
+
+    Use descriptors to override abstract class properties.
+    """
+
+    @classmethod
+    def print_success(cls, script_name: str):
+        print("\n" + f"Execution of {script_name} succeed" + "\n")
+
+    @classmethod
+    def print_failure(cls, script_name: str):
+        print("\n" + f"Execution of {script_name} failed" + "\n")
+
+    @property
+    @abc.abstractclassmethod
+    def stdin(cls):
+        return cls.stdin
+
+    @property
+    @abc.abstractclassmethod
+    def stdout(cls):
+        return cls.stdout
+
+    @property
+    @abc.abstractclassmethod
+    def stderr(cls):
+        return cls.stderr
+
+    @property
+    @abc.abstractclassmethod
+    def command_line_flag(cls):
+        """Flag that user passes at script execution, example: python start.py -s
+        it is used for app to know which OutputInputController
+        child should be used"""
+        return "-s"
+
+
+class TerminalOutputDescriptor(BaseDescriptor):
+    def __set__(self, instance, value_tuple):
+        _shell, value = value_tuple
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, cls):
+        return instance.__dict__[self.name]
+
+
+class TerminalInputDescriptor(BaseDescriptor):
+    def __set__(self, instance, value_tuple):
+        """When revoked ask user for input,
+        instead of using value_tuple parameter"""
+
+        value = input("\n" + "Input: ")
+
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, cls):
+        return instance.__dict__[self.name]
+
+
+class TerminalErrorDescriptor(BaseDescriptor):
+    def __set__(self, instance, value_tuple):
+        _shell, value = value_tuple
+        print("ERROR\n" + " " * 4 + f"{value}" + "\nERROR")
+
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, cls):
+        return instance.__dict__[self.name]
+
+
+class TerminalOutputInput(OutputInputController):
+    stdin = TerminalInputDescriptor()
+    stdout = TerminalOutputDescriptor()
+    stderr = TerminalErrorDescriptor()
--- src/shell.py
+++ src/shell.py
@@ -8,7 +8,8 @@ Environment, in which scripts will be executed in
                                                   before script execution
 
 """
-import pathlib
+from pathlib import Path
+
 import pexpect
 
 from src.exceptions import (
@@ -24,9 +25,10 @@ from src.output import OutputInput
 
 
 def xyz_required(question: str) -> bool:
+    """Ask question until correct choice is not provided"""
     input_invalid = True
     while input_invalid:
-        ask_for_input = input(question + " ([y]/n)" + " " * 4)
+        ask_for_input = input(question + " ([y]/n)" + " " * 2)
         if not bool(ask_for_input):
             ask_for_input = "y"
         if ask_for_input in (
@@ -48,14 +50,34 @@ def termination_required() -> bool:
     return xyz_required("Would You like to terminate script execution?")
 
 
+class _ErrorTempFile:
+    FILE_NAME = "errors_temp.log"
+
+    @classmethod
+    def read_errors(cls):
+        """Read errors from file and clean buffer"""
+        with open(cls.FILE_NAME, "r+") as temp_errors:
+            error_content = temp_errors.read()
+            # Move pointer to file beginning
+            temp_errors.seek(0)
+            # Resize file to pointer position
+            temp_errors.truncate()
+        return error_content
+
+    @classmethod
+    def errors_exist(cls):
+        """If there is any content inside temp file return True"""
+        with open(cls.FILE_NAME) as temp_errors:
+            # Delete whitespaces which could be misleading
+            #   for bool function
+            return bool(temp_errors.read().strip())
+
+
 class Shell:
     """Shell module is responsible for spawning the environment
     and for communicating with it."""
 
-    # Temporary file handling errors content
-    ERROR_FILE_NAME = "errors.file"
-
-    def __init__(self, path: pathlib.Path):
+    def __init__(self, path: Path):
         """path should be pointing to executable shell
         like /usr/bin/python3"""
         if not path.exists():
@@ -71,49 +93,45 @@ class Shell:
         """Spawn shell using self.path, and execute script within it."""
         self.process = pexpect.spawn(
             str(self.path),
-            args=[str(script.path), f"2> {self.ERROR_FILE_NAME}"],
+            args=[str(script.path), f"2> {_ErrorTempFile.FILE_NAME}"],
             encoding="utf-8",
         )
 
-    def _send_command(self, command: str):
+    def send_command(self, command: str):
         self.process.sendline(command)
+
+    def terminate(self):
+        self.process.terminate()
 
     def execute_script(self, script: Script, output_input: OutputInput, timeout=30):
         self.spawn_shell(script)
         while self.process.isalive():
-            # Read output from script
             try:
-                output_input.write_output(self._read_output(timeout))
-            except NoOutputProduced:
-                output_input.write_output(
-                    f"There where no output produced by {script.name}"
-                )
-            # Print output to destinations
-            output_input.print_output()
+                # Pass shell, to allow controll of process by output_input
+                #       for future output analysis extending
+                output_input.stdout = (self, self._read_output(timeout))
+            except NoOutputProduced as err:
+                output_input.stdout = (self, err.args[0])
 
-            # If process is sleeping assume it needs human interaction
-            if Process.is_sleeping(self.process.pid):
-
-                if input_required():
-                    output_input.ask_for_input()
-                    self._send_command(output_input.std_input)
-                elif termination_required():
-                    self.process.terminate()
+            if _ErrorTempFile.errors_exist():
+                # Pass shell, to allow controll of process by output_input
+                #       for future extending error response
+                output_input.stderr = (self, _ErrorTempFile.read_errors())
 
         if self.process.status == 0:
             output_input.print_success(script.name)
         else:
             output_input.print_failure(script.name)
 
-    def _read_output(self, timeout=30) -> str:
+    def _read_output(self, script_name: str, timeout=30) -> str:
         """Read all output lines from shell. If output is not
         recived before timeout return what left"""
         try:
             self.process.expect(pexpect.EOF, timeout=timeout)
-        except pexpect.TIMEOUT:
+        except pexpect.TIMEOUT as err:
             if not self.process.before:
                 raise NoOutputProduced(
-                    "There is no output produced from {self.script.name}"
-                )
+                    "There is no output produced by {script_name}"
+                ) from err
 
         return self.process.before
