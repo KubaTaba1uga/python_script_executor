6c9e209bac21d2fbacf08fe75f9ca2860561ab5f
KubaTaba1uga <jakub@buczynski.net.pl>
2022-02-01 13:14:53 +0100

    cleanup

--- tests/test_4_process.py
+++ tests/test_4_process.py
@@ -1,4 +1,3 @@
-from subprocess import Popen
 from time import sleep
 
 from tests.fixtures import popen_process
@@ -8,9 +7,11 @@ from src.process import Process
 
 def test_is_alive(popen_process):
     assert Process.is_alive(popen_process.pid) is True
+    popen_process.terminate()
 
 
 def test_is_sleeping(popen_process):
+    sleep(0.1)
     assert Process.is_sleeping(popen_process.pid) is True
 
 
