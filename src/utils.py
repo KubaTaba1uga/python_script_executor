a985cc6c889cd147cbeed86925df050fc668392f
KubaTaba1uga <jakub@buczynski.net.pl>
2022-02-01 18:53:41 +0100

    add docstrings

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,30 @@
+""" Utilities for printing messages  to user"""
+
 from colorama import Fore, Style
 
 import sys
 
+INDENT = " " * 4
+
 
 def format_info(output: str) -> str:
+    """Format output on GREEN"""
     return Fore.GREEN + output + Style.RESET_ALL
 
 
 def format_error(output: str) -> str:
+    """Format output on RED"""
     return Fore.RED + output + Style.RESET_ALL
 
 
 def format_success(output: str) -> str:
+    """Format output on BLUE"""
     return Fore.BLUE + output + Style.RESET_ALL
 
 
 def format_indent(output: str) -> str:
-    return " " * 4 + output
+    """Add tab before output"""
+    return INDENT + output
 
 
 def print_(output: str):
@@ -25,12 +33,15 @@ def print_(output: str):
 
 
 def print_info(output: str):
+    """Print  informational notification on GREEN"""
     print_(format_info(output))
 
 
 def print_error(output: str):
+    """Print error notification on RED"""
     print_(format_error(output))
 
 
 def print_success(output: str):
+    """Print success notification on BLUE"""
     print_(format_success(output))
