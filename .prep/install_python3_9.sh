38e8cf200c783b6ea6d793253cd132482d09a73b
KubaTaba1uga <jakub@buczynski.net.pl>
2022-02-12 13:53:00 +0100

    add script for python 3.9 installation

---
+++ .prep/install_python3_9.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+apt-get install wget build-essential libreadline-gplv2-dev  ca-certificates libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
+wget https://github.com/actions/python-versions/releases/download/3.9.8-118810/python-3.9.8-linux-18.04-x64.tar.gz
+tar xzf python-3.9.8-linux-18.04-x64.tar.gz
+tar xzf Python-3.9.8.tgz
+cd Python-3.9.8
+./configure --enable-optimizations  
+make install   
+rm -r -f ../Python-3.9.8
